dc749758dba02e3b372a14d482cb1c6671825ea6
ruggsea <rmlazza@gmail.com>
2024-08-29 02:16:22 +0200

    More selenium

--- llm_poll_parser/website_getter.py
+++ llm_poll_parser/website_getter.py
@@ -1,23 +1,204 @@
 from selenium import webdriver
+from bs4 import BeautifulSoup
+import re, time
 
-# Create a new instance of the Chrome driver
-driver = webdriver.Firefox()
 
-# Open the website
-driver.get('https://www.sondaggipoliticoelettorali.it/Home.aspx?st=HOME')
 
-# Find the "sondaggi" link by its text and click on it
-sondaggi_link = driver.find_element('link text', 'Sondaggi')
-sondaggi_link.click()
+def start_driver():
+    # Create a new instance of the Chrome driver
+    driver = webdriver.Firefox()
+    # Open the website
+    driver.get('https://www.sondaggipoliticoelettorali.it/Home.aspx?st=HOME')
 
-def find_and_print_sondaggi_table(driver):
+    # Find the "sondaggi" link by its text and click on it
+    sondaggi_link = driver.find_element('link text', 'Sondaggi')
+    sondaggi_link.click()
+    
+    return driver
+
+
+def extract_table_data(html_content):
+    soup = BeautifulSoup(html_content, 'html.parser')
+
+    # Initialize list to hold data
+    data_list = []
+
+    # Find all input elements
+    inputs = soup.find_all('input')
+
+    # Initialize dictionary to hold current row data
+    current_row_data = {}
+
+    # Define patterns to identify data row inputs
+    for input_elem in inputs:
+        # get id
+        input_id = input_elem.get('id')
+        # first check if the input is a data row input by checking if id contains dgSondaggi_Row
+        if 'dgSondaggi_Row' not in input_id:
+            continue
+        # get title
+        input_title = input_elem.get('title')
+        # get field name, it's the last part of the id after the last underscore
+        field_name = input_id.split('_')[-1]
+        # row number is the one or two digits before the last underscore
+        row_number = int(input_id.split('_')[-2].replace('Row', ''))
+        if field_name == 'DataInserimento':
+            if current_row_data:
+                data_list.append(current_row_data)
+            current_row_data = {'Row': row_number, 'Data Inserimento': input_title}
+        elif field_name == 'Realizzatore':
+            current_row_data['Realizzatore'] = input_title
+        elif field_name == 'Committente':
+            current_row_data['Committente'] = input_title
+    
+    # Append the last row if it exists
+    if current_row_data:
+        data_list.append(current_row_data)
+
+    # now get the Titolo, all of them are in a td 
+    tds = soup.find_all('td')
+    titles = []
+    for td in tds:
+        title = td.get_text()
+        # get rid of newlines and \t
+        title = title.replace('\t', '')
+        title = title.replace('\n', '')
+        # skip it if it's empty or contains Pagina
+        if not title or 'Pagina' in title:
+            continue
+        
+        titles.append(title)
+    
+    # Add the titles to the data list
+    for i, data_list_item in enumerate(data_list):
+        data_list_item['Titolo'] = titles[i]
+        
+    return data_list
+        
+def find_sondaggi_table(driver):
     # Find the table element containing the sondaggi
     table = driver.find_element('id', 'lista')
 
     # Print the table content
-    print(table)
+    html_content = table.get_attribute('innerHTML')
+    
+    return extract_table_data(html_content)
 
-# Find and print the sondaggi table
-find_and_print_sondaggi_table(driver)
-# # Close the browser
-# driver.quit()
+def rows_intenzioni_di_voto(driver, table):
+    rows_to_click = []
+    # go over the table and find which rows title contains "intenzione di voto" or "sondaggio politico"
+    for row in table:
+        if 'intenzioni di voto' in row['Titolo'].lower() or 'sondaggio su elezioni politiche' in row['Titolo'].lower():
+            # return the row number and data inserimento
+            rows_to_click.append(row['Row'])
+    return rows_to_click
+
+def click_on_row(driver, row):
+    # find the input element to click, it is an eliment the id of which contains dgSondaggi_Row{row}_DataInserimento
+    input_elem=driver.find_element('id', f'ctl00_Contenuto_dgSondaggi_Row{row}_DataInserimento')
+    # click on it once
+    input_elem.click()
+    # wait for the page to load
+    driver.implicitly_wait(2)
+    
+def click_on_domande(driver):
+    # find the domande element, it has id ctl00_Titolo_TabSondaggio_DomandeRisposte
+    domande_elem = driver.find_element('id', 'ctl00_Titolo_TabSondaggio_DomandeRisposte')
+    domande_elem.click()
+    
+
+def get_lista_domande(driver):
+    # we want a list of the elements that contain the domande, their id look like ctl00_Contenuto_ucGestioneDomande_ucListaDomande_dgDomande_Row1_Domanda
+    domande=[]
+    for i in range(1, 100):
+        try:
+            domanda_elem = driver.find_element('id', f'ctl00_Contenuto_ucGestioneDomande_ucListaDomande_dgDomande_Row{i}_Domanda')
+            # get the title of the domanda
+            domanda_title = domanda_elem.get_attribute('title')
+            print(domanda_title)
+            domande.append(domanda_elem)
+        except:
+            break
+    return domande
+
+def get_right_domanda(driver, domande):
+    # we want to find the domanda that contains the word "intenzione di voto" or "sondaggio politico"
+    for domanda in domande:
+        titolo_domanda = domanda.get_attribute('title')
+        if 'elezioni nazionali' in titolo_domanda.lower() or 'intenzioni di voto' in titolo_domanda.lower() or "elezioni politiche" in titolo_domanda.lower() or "votasse oggi" in titolo_domanda.lower():
+            # find the element again and click on it
+            domanda_to_click = driver.find_element('id', domanda.get_attribute('id'))
+            domanda_to_click.click()
+            print(f'Clicked on {titolo_domanda}')
+            break
+        
+def get_testo_risposta(driver):
+    # find the element that contains the testo risposta with id ctl00_Contenuto_ucGestioneDomande_ucSchedaDomandaReadOnly_Risposta
+    testo_risposta_elem = driver.find_element('id', 'ctl00_Contenuto_ucGestioneDomande_ucSchedaDomandaReadOnly_Risposta')
+    # get the text
+    testo_risposta = testo_risposta_elem.text
+    return testo_risposta       
+
+def parse_allegato_table(html_content):
+    soup = BeautifulSoup(html_content, 'html.parser')
+    
+    # Find the table
+    table = soup.find('table', {'class': 'lista'})
+    
+    # Initialize dictionary to hold data
+    table_data = {}
+
+    # Iterate over each row in the table
+    for row in table.find_all('tr'):
+        cols = row.find_all('td')
+        # Ensure there are two columns
+        if len(cols) == 2:
+            key = cols[0].get_text(strip=True)
+            value = cols[1].get_text(strip=True)
+            # Add to the dictionary
+            table_data[key] = value
+    
+    # check if the table is empty string
+    if not table_data:
+        return None
+    else:
+        return table_data
+        
+
+def get_poll_data(driver):
+    # Find the table containing the sondaggi
+    table = find_sondaggi_table(driver)
+    
+    # Find the rows that contain intenzioni di voto
+    rows_to_click = rows_intenzioni_di_voto(driver, table)
+    
+    # Click on first row
+    click_on_row(driver, rows_to_click[0])
+    
+    # Click on domande
+    click_on_domande(driver)
+    time.sleep(2)
+    domande=get_lista_domande(driver)
+    get_right_domanda(driver, domande)
+    time.sleep(2)
+    
+    # Get risposta
+    testo_risposta = get_testo_risposta(driver)
+    print(testo_risposta)
+    attachment_html = driver.page_source  
+    
+    # Extract and parse the allegato table
+    allegato_data = parse_allegato_table(attachment_html)
+    
+    if allegato_data:
+        print("Allegato Data:", allegato_data)
+    else:
+        print("No data extracted from allegato table.")
+
+    
+if __name__ == "__main__":
+    driver = start_driver()
+    get_poll_data(driver)
+
+    # Close the driver
+    driver.quit()
